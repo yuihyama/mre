e2016842403426dac69a805dd01d58a0ead93295
yuihyama <yuihyama@gmail.com>
2018-08-08 01:28:13 +0900

    Add file

---
+++ at/at.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+ary = [1, 'a', 2, 'b', 'c']
+
+p ary.at(0)
+puts ary.at(0)
+puts
+
+ary.each_index { |i| p ary.at(i) }
+puts
+
+ary.each_index do |i|
+  puts ary.at(i)
+end
+puts
+
+(0...ary.size).each { |i| p ary.at(i) }
+puts
+
+(0...ary.length).each do |i|
+  puts ary.at(i)
+end
