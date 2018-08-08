de124143f42c098dbefc17a831cfe4ae95e014cf
yuihyama <yuihyama@gmail.com>
2018-08-08 19:53:39 +0900

    Add file

---
+++ combination/combination.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+# enum.combination(size) -> enumerator
+# enum.combination(size) { |array| block } -> enum
+
+ary = ['a', 'b', 'c']
+
+p ary.combination(2).to_a
+puts
+puts ary.combination(2).to_a
+puts
+print ary.combination(2).to_a, "\n"
+puts
+
+ary.combination(2) { |a| p a }
+puts
+
+ary.combination(2) do |a|
+  puts a
+end
+puts
+
+ary.combination(2) do |a|
+  print a, "\n"
+end
+puts
+
+ary.each_index do |i|
+  print ary.combination(i).to_a, "\n"
+end
+puts
+
+(0..ary.length).each do |i|
+  print ary.combination(i).to_a, "\n"
+end
+puts
+
+(0..ary.size+1).each do |i|
+  print ary.combination(i).to_a, "\n"
+end
