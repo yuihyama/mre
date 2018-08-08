ebe3f733e6ec0f242431e269c26c15ae54f728f5
yuihyama <yuihyama@gmail.com>
2018-08-08 15:20:05 +0900

    Add file

---
+++ clear/clear.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# enum.clear -> enum
+
+ary = %w[a b c d]
+p ary
+puts ary
+puts
+
+p ary.clear
+puts ary.clear
+puts
+
+p ary
+puts ary
+puts
+
+p [1]
+puts [1]
+puts
+
+p [1].clear
+puts [1].clear
