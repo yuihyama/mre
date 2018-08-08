0dad83e48d0de6e735b54de52efe9f2ecac21fa1
yuihyama <yuihyama@gmail.com>
2018-08-09 02:00:09 +0900

    Add file

---
+++ collect-excl/collect_excl.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# enum.collect! { |obj| block } -> enum
+
+ary1 = %w[a b c d]
+ary1.collect! { |c| c + '?!' }
+p ary1
+puts ary1
+puts
+
+ary2 = %w[a b c d]
+ary2.collect! { |c| c + '!?' }
+p ary2
+puts ary2
+puts
+
+ary3 = [1, 2, 3]
+ary3.collect! { |n| n ** 2}
+p ary3
+puts ary3
