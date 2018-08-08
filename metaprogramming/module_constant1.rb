9319e16a3bc974b0e003cf8dae1b76714605f0b6
yuihyama <yuihyama@gmail.com>
2018-08-08 20:25:36 +0900

    Add file

---
+++ metaprogramming/module_constant1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+module MyModule
+  CONST = 'outside'
+
+  class MyClass
+    CONST = 'inside'
+  end
+end
+
+p MyModule::CONST
+puts MyModule::CONST
+puts
+
+p MyModule::MyClass::CONST
+puts MyModule::MyClass::CONST
